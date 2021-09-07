6421d354f5255453766210e057f1aea994b4fb21
Jordan Pellizzari <jpellizzari21@gmail.com>
2021-09-06 19:11:19 -0700

    Add persistence

--- Scenery2.lua
+++ Scenery2.lua
@@ -11,11 +11,23 @@ do
         ["REPORT_FILENAME"] = "scenery_strike_report.csv",
         ["PERSISTENT"] = true,
     }
+
+    local columns = {
+        "targetZoneName",
+        "dead",
+    }
     
     -- [<scenery object id] = <tgt zone name>
     local buildingLookup = {}
+    -- [<tgt zone name>] = <mission zone name>
     local targetToMissionLookup = {}
+    -- [<tgt zone name>] = <destroyed bool>
+    local destroyedBuildings = {}
+    -- 
     local internalConfig = {}
+
+    local eventHandlers = {}
+    
 
     local function log(tmpl, ...)
         local txt = string.format("[SCENE] " .. tmpl, ...)
@@ -43,6 +55,16 @@ do
         return cfg
     end
 
+    local function makeRecord(zoneName, destroyed)
+        local record = {}
+        for i,col in ipairs(columns) do
+            record[col] = nil
+        end
+
+        record.targetZoneName = zoneName
+        record.destroyed = destroyed
+    end
+
     local function getReportFile()
         local fileName = string.format("%s\\%s", lfs.writedir(), internalConfig.REPORT_FILENAME)
         local fp = io.open(fileName, 'w')
@@ -59,8 +81,8 @@ do
         end
 
         local csv = ""
-        for id,tgtZoneName in pairs(buildingLookup) do
-            local row = string.format("%s,%s\n", zone, status)
+        for tgtZoneName,destroyed in pairs(destroyedBuildings) do
+            local row = string.format("%s,%s\n", tgtZoneName, destroyed and "true" or "false")
             csv = csv .. row
         end
 
@@ -76,11 +98,12 @@ do
         end
 
         if event.id == world.event.S_EVENT_DEAD and object:getCategory() == Object.Category.SCENERY then
-            local zone = buildingLookup[object:getName()]
+            local zone = buildingLookup[string.format("%s", object:getName())]
 
             if zone then
-                
-
+                log("Zone %s", zone)
+                destroyedBuildings[zone] = true
+                writeReport()
                 for i,handler in ipairs(eventHandlers) do
                     handler(zone)
                 end
@@ -137,27 +160,48 @@ do
         trigger.action.outTextForGroup(params.groupID, str, 90)
     end
 
-    function scenery.init()
-        internalConfig = buildConfig()
-        local missionZones, targetZones = findZones()
-        local markId = 1
+    local function getReportFile(writeAccess)
+        local fileName = string.format("%s\\%s", lfs.writedir(), internalConfig.REPORT_FILENAME)
+        local file = io.open(fileName, writeAccess and 'w' or 'r')
 
-        if internalConfig.RENDER_MENUS then
-            local m = {}
-            for name, zone in pairs(missionZones) do
-                local title = name:gsub("%b[]", "")
-                table.insert(m, { title = title, func = printMissionInfo, params = { name = title, zone = zone } } )
+        return file
+    end
+
+    local function readState(text)
+        local pattern = ""
+        local state = {}
+        for i,col in ipairs(columns) do
+            local s = i == #columns and "(.*)" or "(.*),"
+            pattern = pattern .. s
+        end
+
+        for row in text:gmatch("[^\r\n]+") do
+            local match = {string.match(row, pattern)}
+            local record = {}
+
+            for i,col in ipairs(columns) do
+                record[col] = match[i]
             end
 
-            menus.menuForAllGroups("Strike Missions", m)
+            table.insert(state, record)
         end
-   
+
+        return state
+    end
+
+    local function generateBuildingLookup(targetZones, missionZones)
+        local markId = 1
         for name,tz in pairs(targetZones) do
-            for i,prop in ipairs(tz.properties) do
-                if prop.key == "OBJECT ID" then
-                    buildingLookup[prop.value] = name
+
+            if tz then
+                -- tz will be nil if the target is already dead
+                for i,prop in ipairs(tz.properties) do
+                    if prop.key == "OBJECT ID" then
+                        buildingLookup[prop.value] = name
+                    end
                 end
             end
+
 
             for name,mz in pairs(missionZones) do
                 if mist.pointInPolygon(tz, mz.verticies) then
@@ -170,12 +214,46 @@ do
                 markId = markId + 1
             end
         end
+    end
 
-        -- for i,zone in ipairs(missionZones) do
-        --     populateMenus(zone, targetZones)
-        -- end
+    function scenery.init()
+        internalConfig = buildConfig()
+        local missionZones, targetZones = findZones()
+        local fp = getReportFile()
 
-        -- mist.addEventHandler(eventHandler)
+        if fp then
+            local text = fp:read("*all")
+            fp:close()
+    
+            if text == "" then
+                log("Error: could not read report file")
+            else
+                local state = readState(text)
+                for i,row in ipairs(state) do
+                    if targetZones[row.targetZoneName] and row.dead then
+                        log("Target zone %s dead, removing...", row.targetZoneName)
+                        targetZones[row.targetZoneName] = nil
+                    end
+                end
+            end
+        else
+            log("Report file %s not found. Generating...", internalConfig.REPORT_FILENAME)
+            writeReport()
+        end
+
+        generateBuildingLookup(targetZones, missionZones)
+
+        if internalConfig.RENDER_MENUS then
+            local m = {}
+            for name, zone in pairs(missionZones) do
+                local title = name:gsub("%b[]", "")
+                table.insert(m, { title = title, func = printMissionInfo, params = { name = title, zone = zone } } )
+            end
+
+            menus.menuForAllGroups("Strike Missions", m)
+        end
+
+        mist.addEventHandler(eventHandler)
         trigger.action.outText("Scenery2 initialized", 30)
     end
 end
